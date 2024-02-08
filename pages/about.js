2e9b9945a756972a66c6e9938bde68bc1c0c29c9
hiroka <mementoka@gmail.com>
2024-02-09 00:23:22 +0900

    fix publications

--- pages/about.js
+++ pages/about.js
@@ -51,12 +51,12 @@ export default function About() {
 
           <h3 className='text-lg font-bold my-2'>論文誌</h3>
           <ol className='list-decimal list-inside text-base m-2'>
-            <li><span className='underline'>H. Oka</span>, A. Ohnishi, T. Terada, M. Tsukamoto: System for Detecting Learner Stuck in Programming Learning, <span className='italic'>Sensors</span>, Vol. 23, No. 12, pp. 5739 (June 2023).</li>
+            <li><span className='underline'>H. Oka</span>, A. Ohnishi, T. Terada, M. Tsukamoto: <Link href="https://www.mdpi.com/1424-8220/23/12/5739" className="text-blue-600 dark:text-blue-500 hover:underline">System for Detecting Learner Stuck in Programming Learning</Link>, <span className='italic'>Sensors</span>, Vol. 23, No. 12, pp. 1-2 (June 2023).</li>
           </ol>
 
           <h3 className='text-lg font-bold my-2'>国際会議</h3>
           <ol className='list-decimal list-inside text-base m-2'>
-            <li><span className='underline'>H. Oka</span>, A. Ohnishi, T. Terada, M. Tsukamoto: A Stumble Detection Method for Programming with Multi-modal Information, In International Conference on Advances in Mobile Computing and Multimedia Intelligence (MoMM2022), pp. 169–174 (Nov. 2022).</li>
+            <li><span className='underline'>H. Oka</span>, A. Ohnishi, T. Terada, M. Tsukamoto: <Link href="https://dl.acm.org/doi/abs/10.1007/978-3-031-20436-4_16" className="text-blue-600 dark:text-blue-500 hover:underline">A Stumble Detection Method for Programming with Multi-modal Information</Link>, <span className='italic'>Proc. of 20th International Conference on Advances in Mobile Computing and Multimedia Intelligence (MoMM2022)</span>, pp. 169--174 (Nov. 2022).</li>
           </ol>
 
             <h3 className='text-lg font-bold my-2'>国内会議</h3>
@@ -70,7 +70,7 @@ export default function About() {
 (Mar. 2022).</li>
             <li><span className='underline'>岡 大貴</span>, 大西鮎美, 寺田 努, 塚本昌彦: プログラミング演習授業のためのつまずき検出システムの実運用, ユビキタス・ウェアラブルワークショップ2022論文集, Vol.2022, p.20 (Dec. 2022).</li>
             <li><span className='underline'>岡 大貴</span>, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦: プログラミング初学者の課題解決方針立案能力の向上に焦点をあてた選択肢タップ式学習システム, 情報処理学会研究報告コンピュータと教育 (CE), Vol.2023-CE-171, No. 14, pp. 1--7 (Oct. 2023).</li>
-            <li><span className='underline'>岡 大貴</span>, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦: プログラミング初学者の課題解決方針の立案能力を高める選択肢タップ式学習支援システム , WISS2023, Vol.2023, No. 100, pp. 34--40 (Nov. 2023).</li>
+            <li><span className='underline'>岡 大貴</span>, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦: <Link href="https://www.wiss.org/WISS2023Proceedings/data/05.pdf" className="text-blue-600 dark:text-blue-500 hover:underline">プログラミング初学者の課題解決方針の立案能力を高める選択肢タップ式学習支援システム</Link>, WISS2023, Vol.2023, No. 100, pp. 34--40 (Nov. 2023).</li>
             <li><span className='underline'>岡 大貴</span>, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦: プログラミング未経験者の興味喚起を目的としたタップ式プログラミングゲームの提案, ユビキタス・ウェアラブルワークショップ2023論文集, Vol.2023,  p. 7 (Dec. 2023).</li>
           </ol>
         <h3 className='text-xl font-bold pt-3'>受賞・予算獲得等</h3>
