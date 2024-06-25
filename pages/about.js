980cf2f2980427fbed4cab62277b585480b07297
hiroka <mementoka@gmail.com>
2024-06-25 10:15:30 +0900

    change structure of paperlists

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,119 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from 'next/head';
+import Link from 'next/link';
 //import Image from 'next/image'
-import Layout, {siteTitle, name} from '../components/layout'
+import Layout, {siteTitle, name} from '../components/layout';
+
+const researchPapers = [
+    {
+        author: "岡 大貴, 寺田 努, 塚本昌彦",
+        title: "プロジェクションマッピングによる読書体験の拡張",
+        publication: "ユビキタス・ウェアラブルワークショップ2018論文集",
+        pages: "p.26",
+        date: "Dec. 2018"
+    },
+    {
+        author: "岡 大貴, 西田健志",
+        title: "ゲーミフィケーションによるコードリーディングの促進",
+        url: 'https://ipsj.ixsq.nii.ac.jp/ej/?action=pages_view_main&active_action=repository_view_main_item_detail&item_id=199459&item_no=1&page_id=13&block_id=8',
+        publication: "エンタテインメントコンピューティングシンポジウム2019論文集",
+        volume: "vol. 2019",
+        pages: "pp. 346--349",
+        date: "Sept. 2019"
+    },
+    {
+        author: "岡 大貴, 西田健志",
+        title: "エンターテイメントを用いたコードリーディング促進手法の提案",
+        publication: "ユビキタス・ウェアラブルワークショップ2019論文集",
+        pages: "p.59",
+        date: "Dec. 2019"
+    },
+    {
+        author: "岡 大貴, 西田健志",
+        title: "競技性・観戦性を拡張したプログラミングゲームの提案",
+        url: 'https://ipsj.ixsq.nii.ac.jp/ej/?action=pages_view_main&active_action=repository_view_main_item_detail&item_id=206553&item_no=1&page_id=13&block_id=8',
+        publication: "エンタテインメントコンピューティングシンポジウム2020論文集",
+        volume: "Vol. 2020",
+        pages: "pp.166--169",
+        date: "Sep. 2020"
+    },
+    {
+        author: "岡 大貴, 大西鮎美, 寺田 努, 塚本昌彦",
+        title: "マルチモーダル情報を用いたプログラミング時のつまずき検出手法の検討",
+        publication: "ユビキタス・ウェアラブルワークショップ2021論文集",
+        volume: "Vol.2021",
+        pages: "p.36",
+        date: "Dec. 2021"
+    },
+    {
+        author: "岡 大貴, 大西鮎美, 寺田 努, 塚本昌彦",
+        title: "マルチモーダル情報を用いたプログラミング時のつまずき検出手法の提案",
+        url: 'https://ipsj.ixsq.nii.ac.jp/ej/?action=pages_view_main&active_action=repository_view_main_item_detail&item_id=217428&item_no=1&page_id=13&block_id=8',
+        publication: "情報処理学会研究報告ヒューマンコンピュータインタラクション (HCI)",
+        volume: "Vol. 2022-HCI-197",
+        number: "No. 9",
+        pages: "pp. 1–7",
+        date: "Mar. 2022"
+    },
+    {
+        author: "岡 大貴, 大西鮎美, 寺田 努, 塚本昌彦",
+        title: "プログラミング演習授業のためのつまずき検出システムの実運用",
+        publication: "ユビキタス・ウェアラブルワークショップ2022論文集",
+        volume: "Vol.2022",
+        pages: "p.20",
+        date: "Dec. 2022"
+    },
+    {
+        author: "岡 大貴, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦",
+        title: "プログラミング初学者の課題解決方針立案能力の向上に焦点をあてた選択肢タップ式学習システム",
+        url: 'https://ipsj.ixsq.nii.ac.jp/ej/?action=pages_view_main&active_action=repository_view_main_item_detail&item_id=228457&item_no=1&page_id=13&block_id=8',
+        publication: "情報処理学会研究報告コンピュータと教育 (CE)",
+        volume: "Vol.2023-CE-171",
+        number: "No. 14",
+        pages: "pp. 1--7",
+        date: "Oct. 2023"
+    },
+    {
+        author: "岡 大貴, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦",
+        title: "プログラミング初学者の課題解決方針の立案能力を高める選択肢タップ式学習支援システム",
+        url: 'https://www.wiss.org/WISS2023Proceedings/data/05.pdf',
+        publication: "WISS2023",
+        volume: "Vol.2023",
+        number: "No. 100",
+        pages: "pp. 34--40",
+        date: "Nov. 2023"
+    },
+    {
+        author: "岡 大貴, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦",
+        title: "プログラミング未経験者の興味喚起を目的としたタップ式プログラミングゲームの提案",
+        publication: "ユビキタス・ウェアラブルワークショップ2023論文集",
+        volume: "Vol.2023",
+        pages: "p. 7",
+        date: "Dec. 2023"
+    },
+    {
+        author: "岡 大貴, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦",
+        title: "プログラマの格好良さを見せることで未経験者のプログラミングに対する意欲を高める対戦ゲーム",
+        url: 'https://ipsj.ixsq.nii.ac.jp/ej/?action=pages_view_main&active_action=repository_view_main_item_detail&item_id=234514&item_no=1&page_id=13&block_id=8',
+        publication: "情報処理学会研究報告エンタテインメントコンピューティング (EC)",
+        volume: "Vol.2024-EC-72",
+        number: "No. 6",
+        pages: "pp. 1--7",
+        date: "May. 2024"
+    }
+];
+
+const linkClass = 'text-blue-600 dark:text-blue-500 hover:underline';
+const ResearchPaper = ({ paper }) => (
+  <li className='research-paper'>
+    {paper.author},
+     {paper.url ? (<Link href={paper.url} className={linkClass}>{paper.title}</Link>) : (paper.title)},
+    {paper.publication},
+    {paper.volume},
+    {paper.number},
+    {paper.pages}
+    ({paper.date}).
+  </li>
+);
 
 export default function About() {
   return (
@@ -51,27 +163,19 @@ export default function About() {
 
           <h3 className='text-lg font-bold my-2'>論文誌</h3>
           <ol className='list-decimal list-inside text-base m-2'>
-            <li><span className='underline'>H. Oka</span>, A. Ohnishi, T. Terada, M. Tsukamoto: <Link href="https://www.mdpi.com/1424-8220/23/12/5739" className="text-blue-600 dark:text-blue-500 hover:underline">System for Detecting Learner Stuck in Programming Learning</Link>, <span className='italic'>Sensors</span>, Vol. 23, No. 12, pp. 1-2 (June 2023).</li>
+            <li><span className='underline'>H. Oka</span>, A. Ohnishi, T. Terada, M. Tsukamoto: <Link href="https://www.mdpi.com/1424-8220/23/12/5739" className={linkClass}>System for Detecting Learner Stuck in Programming Learning</Link>, <span className='italic'>Sensors</span>, Vol. 23, No. 12, pp. 1-2 (June 2023).</li>
           </ol>
 
           <h3 className='text-lg font-bold my-2'>国際会議</h3>
           <ol className='list-decimal list-inside text-base m-2'>
-            <li><span className='underline'>H. Oka</span>, A. Ohnishi, T. Terada, M. Tsukamoto: <Link href="https://dl.acm.org/doi/abs/10.1007/978-3-031-20436-4_16" className="text-blue-600 dark:text-blue-500 hover:underline">A Stumble Detection Method for Programming with Multi-modal Information</Link>, <span className='italic'>Proc. of 20th International Conference on Advances in Mobile Computing and Multimedia Intelligence (MoMM2022)</span>, pp. 169--174 (Nov. 2022).</li>
+            <li><span className='underline'>H. Oka</span>, A. Ohnishi, T. Terada, M. Tsukamoto: <Link href="https://dl.acm.org/doi/abs/10.1007/978-3-031-20436-4_16" className={linkClass}>A Stumble Detection Method for Programming with Multi-modal Information</Link>, <span className='italic'>Proc. of 20th International Conference on Advances in Mobile Computing and Multimedia Intelligence (MoMM2022)</span>, pp. 169--174 (Nov. 2022).</li>
           </ol>
 
             <h3 className='text-lg font-bold my-2'>国内会議</h3>
             <ol className='list-decimal list-inside m-2 text-base'>
-              <li><span className='underline'>岡 大貴</span>, 寺田 努, 塚本昌彦: プロジェクションマッピングによる読書体験の拡張, ユビキタス・ウェアラブルワークショップ2018論文集, p.26 (Dec. 2018).</li>
-              <li><span className='underline'>岡 大貴</span>, 西田健志: ゲーミフィケーションによるコードリーディングの促進, エンタテインメントコンピューティングシンポジウム2019論文集, vol. 2019, pp. 346--349 (Sept.2019).</li>
-              <li><span className='underline'>岡 大貴</span>, 西田健志: エンターテイメントを用いたコードリーディング促進手法の提案, ユビキタス・ウェアラブルワークショップ2019論文集, p.59 (Dec.2019).</li>
-              <li><span className='underline'>岡 大貴</span>, 西田健志: 競技性・観戦性を拡張したプログラミングゲームの提案, エンタテインメントコンピューティングシンポジウム2020論文集, Vol. 2020, pp.166--169 (Sep. 2020).</li>
-              <li><span className='underline'>岡 大貴</span>, 大西鮎美, 寺田 努, 塚本昌彦: マルチモーダル情報を用いたプログラミング時のつまずき検出手法の検討, ユビキタス・ウェアラブルワークショップ2021論文集, Vol.2021, p.36 (Dec. 2021).</li>
-              <li><span className='underline'>岡 大貴</span>, 大西鮎美，寺田 努, 塚本昌彦: マルチモーダル情報を用いたプログラミング時のつまずき検出手法の提案, 情報処理学会研究報告ヒューマンコンピュータインタラクション (HCI), Vol. 2022-HCI-197, No. 9, pp. 1–7
-(Mar. 2022).</li>
-            <li><span className='underline'>岡 大貴</span>, 大西鮎美, 寺田 努, 塚本昌彦: プログラミング演習授業のためのつまずき検出システムの実運用, ユビキタス・ウェアラブルワークショップ2022論文集, Vol.2022, p.20 (Dec. 2022).</li>
-            <li><span className='underline'>岡 大貴</span>, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦: プログラミング初学者の課題解決方針立案能力の向上に焦点をあてた選択肢タップ式学習システム, 情報処理学会研究報告コンピュータと教育 (CE), Vol.2023-CE-171, No. 14, pp. 1--7 (Oct. 2023).</li>
-            <li><span className='underline'>岡 大貴</span>, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦: <Link href="https://www.wiss.org/WISS2023Proceedings/data/05.pdf" className="text-blue-600 dark:text-blue-500 hover:underline">プログラミング初学者の課題解決方針の立案能力を高める選択肢タップ式学習支援システム</Link>, WISS2023, Vol.2023, No. 100, pp. 34--40 (Nov. 2023).</li>
-            <li><span className='underline'>岡 大貴</span>, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦: プログラミング未経験者の興味喚起を目的としたタップ式プログラミングゲームの提案, ユビキタス・ウェアラブルワークショップ2023論文集, Vol.2023,  p. 7 (Dec. 2023).</li>
+              {researchPapers.map((paper, index) => (
+                  <ResearchPaper key={index} paper={paper} />
+              ))}
           </ol>
         <h3 className='text-xl font-bold pt-3'>受賞・予算獲得等</h3>
         <ul className='list-disc list-inside mx-3 text-base'>
