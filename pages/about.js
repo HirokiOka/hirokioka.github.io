fc9b687242aba9f53531b10e1f56eaaeef5eaabc
hiroka <mementoka@gmail.com>
2024-09-07 14:51:40 +0900

    fix

--- pages/about.js
+++ pages/about.js
@@ -61,9 +61,18 @@ const researchPapers = [
         title: "ゲーミフィケーションによるコードリーディングの促進",
         url: 'https://ipsj.ixsq.nii.ac.jp/ej/?action=pages_view_main&active_action=repository_view_main_item_detail&item_id=199459&item_no=1&page_id=13&block_id=8',
         publication: "エンタテインメントコンピューティングシンポジウム2019論文集",
-        volume: "vol. 2019",
+        volume: "Vol. 2019",
         pages: "pp. 346--349",
         date: "Sept. 2019"
+    },
+    {
+        author: "井上大雅, 岡 大貴, 大西鮎美, 西田健志, 寺田 努, 塚本昌彦",
+        title: "情報技術を活用して誰でも楽しめるスポーツを実現するルール設計プロセス及びその詳細考察",
+        url: 'https://ipsj.ixsq.nii.ac.jp/ej/?action=pages_view_main&active_action=repository_view_main_item_detail&item_id=238705&item_no=1&page_id=13&block_id=8',
+        publication: "エンタテインメントコンピューティングシンポジウム2024論文集",
+        volume: "Vol. 2024",
+        pages: "pp. 113--122",
+        date: "Aug. 2024"
     },
 ];
 
@@ -170,7 +179,7 @@ export default function About() {
           <h3 className='text-lg font-bold mx-4'>論文誌</h3>
           <ol className='list-decimal list-inside text-base mx-8'>
             <li><span className='underline'>H. Oka</span>, A. Ohnishi, T. Terada, M. Tsukamoto: <Link href="https://www.mdpi.com/1424-8220/23/12/5739" className={linkClass}>System for Detecting Learner Stuck in Programming Learning</Link>, <span className='italic'>Sensors</span>, Vol. 23, No. 12, pp. 1-2 (June 2023).</li>
-            <li><span className='underline'>H. Oka</span>, A. Ohnishi, T. Terada, M. Tsukamoto:<Link href="https://ieeexplore.ieee.org/document/10637432" className={linkClass}>A Choice-based Programming Learning Method to Develop Problem-Solving Skills</Link>, <span className='italic'>IEEE Access</span>, pp. 1--13 (Aug. 2024)</li>
+            <li><span className='underline'>H. Oka</span>, A. Ohnishi, T. Terada, M. Tsukamoto:<Link href="https://ieeexplore.ieee.org/document/10637432" className={linkClass}>A Choice-based Programming Learning Method to Develop Problem-Solving Skills</Link>, <span className='italic'>IEEE Access</span>, Vol. 12, pp. 119550 -- 119562 (Aug. 2024)</li>
           </ol>
 
           <h3 className='text-lg font-bold mx-4 mt-2'>国際会議</h3>
@@ -202,11 +211,11 @@ export default function About() {
 
         <h3 className='text-xl font-bold pt-4'>受賞・予算獲得等</h3>
         <ul className='list-disc list-inside mx-8 text-base'>
-          <li>令和４年度神戸大学博士学生フェロー</li>
+          <li><span className='italic'>IWIN2024</span> Best Presentation Award</li>
+          <li>令和5年度日本学生支援機構奨学金 特に優れた業績による返還免除</li>
+          <li>令和4年度神戸大学博士学生フェロー</li>
           <li className={linkClass}><a href='https://hacku.yahoo.co.jp/hacku2021online2/'>Yahoo!JAPAN ハッカソン Open Hack U 2021 Online Vol.2 優秀賞 (つぶやき書店)</a></li>
           <li>令和2年度日本学生支援機構奨学金 特に優れた業績による返還免除</li>
-          <li>令和5年度日本学生支援機構奨学金 特に優れた業績による返還免除</li>
-          <li>IWIN2024 Best Presentation Award</li>
         </ul>
           
     </div>
