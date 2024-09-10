dfa419b36c7cb7f18fd6341317de29a392720667
hiroka <mementoka@gmail.com>
2024-09-10 11:33:38 +0900

    fix link to ProgTouch

--- pages/work.js
+++ pages/work.js
@@ -39,7 +39,7 @@ export default function Projects() {
                 title="ProgTouch"
                 description="自然言語の選択によりスマホからプログラミングを学習するWebアプリケーション"
                 imgSrc="/images/ProgTouch_img.png"
-                url="https://prog-touch.vercel.app/"
+                url="https://github.com/HirokiOka/prog-touch"
                 tags={['TypeScript', 'React', 'Next.js', 'PostgreSQL']}
               />
               <WorkCard 
